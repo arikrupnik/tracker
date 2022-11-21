82f69199c05b079435a1dad5b5cb4f5dd2ae24b3
Ari Krupnik <ari@30pins.com>
2022-11-20 20:17:36 -0600

    extracting compositions into formal functions; PT and AzEl are basically the same as far as math goes

--- euler_conventions.py
+++ euler_conventions.py
@@ -30,8 +30,9 @@ def test_RPY():
     # SciPy normalizes the representation:
     assert str(RPY(  0, 100,   0)) == "r: 180.0 p: 80.0 y: 180.0"
 
+
 class AzEl():
-    """Euler angles as Azimuth/Elevation, extrinsic rotations with respect
+    """Euler angles as Azimuth/Elevation, intrinsic rotations with respect
        to North and horizon."""
     def __init__(self, azimuth=None, elevation=None, *, r=None):
         if r is None:
@@ -41,10 +42,12 @@ class AzEl():
         self.azimuth, self.elevation, _ = self.r.as_euler("ZYX", degrees=True)
     def __str__(self):
         return f"a:{self.azimuth:6.1f} e:{self.elevation:5.1f}"
+
 def test_AzEl():
     assert str(AzEl(  20,  30)) == "a:  20.0 e: 30.0"
     assert str(AzEl( 120,  30)) == "a: 120.0 e: 30.0"
     assert str(AzEl(-120, -30)) == "a:-120.0 e:-30.0"
+
 
 class PT():
     """Euler angles as Pan/Tilt, intrinsic rotations with respect to
@@ -57,59 +60,60 @@ class PT():
         self.pan, self.tilt, _ = self.r.as_euler("ZYX", degrees=True)
     def __str__(self):
         return f"p:{self.pan:6.1f} t:{self.tilt:5.1f}"
+
 def test_PT():
     assert str(PT(  20,  30)) == "p:  20.0 t: 30.0"
     assert str(PT( 120,  30)) == "p: 120.0 t: 30.0"
     assert str(PT(-120, -30)) == "p:-120.0 t:-30.0"
 
-def test_composition():
-    # find azimuth/elevation of camera from base attitude and pan/tilt of
-    # turret
 
-    def compose(base_att, turret_pt):
-        return AzEl(r=(base_att.r * turret_pt.r))
+def resultant_azel(base_rpy, turret_pt):
+    """Computes resulting Azimuth/Alevation of camera from base attitude
+       and Pan/Tilt of turret."""
+    return AzEl(r=(base_rpy.r * turret_pt.r))
 
+def test_resultant_azel():
     # simple addition on one axis
     att = RPY(0, 30, 0)
     pt = PT(0, 20)
-    assert str(compose(att, pt)) == "a:   0.0 e: 50.0"
+    assert str(resultant_azel(att, pt)) == "a:   0.0 e: 50.0"
 
     # rotating the base rotates the turret
     att = RPY(0, 0, 90)
-    assert str(compose(att, pt)) == "a:  90.0 e: 20.0"
+    assert str(resultant_azel(att, pt)) == "a:  90.0 e: 20.0"
 
     # yawing the base right and pitching it up adds turret tilt to base pitch
     att = RPY(0, 30, 90)
-    assert str(compose(att, pt)) == "a:  90.0 e: 50.0"
+    assert str(resultant_azel(att, pt)) == "a:  90.0 e: 50.0"
 
     # yawing the base right and rolling it left decreases azimuth and elevation
     att = RPY(-30, 0, 90)
-    assert str(compose(att, pt)) == "a:  79.7 e: 17.2"
+    assert str(resultant_azel(att, pt)) == "a:  79.7 e: 17.2"
 
-def test_inv_composition():
-    # find necessary pan/tilt for turret from base attitude and target
-    # azimuth/elevation
 
-    def compose(base_att, target_azel):
-        return PT(r=(base_att.r.inv() * target_azel.r))
+def necessary_pt(base_rpy, target_azel):
+    """Computes necessary Pan/Tilt for turret from base attitude to point
+       at target Azimuth/Elevation."""
+    return PT(r=(base_rpy.r.inv() * target_azel.r))
 
+def test_necessary_pt():
     # simple subtraction on one axis
     att = RPY(0, 30, 0)
     azel = AzEl(0, 20)
-    assert str(compose(att, azel)) == "p:   0.0 t:-10.0"
+    assert str(necessary_pt(att, azel)) == "p:   0.0 t:-10.0"
 
     # yawing the base requires unyawing the turret but has no effect on tilt
     att = RPY(0, 0, 90)
-    assert str(compose(att, azel)) == "p: -90.0 t: 20.0"
+    assert str(necessary_pt(att, azel)) == "p: -90.0 t: 20.0"
 
     # yawing the base right and rollig it right subtracts base roll from target
     # elevation
     att = RPY(30, 0, 90)
-    assert str(compose(att, azel)) == "p: -90.0 t:-10.0"
+    assert str(necessary_pt(att, azel)) == "p: -90.0 t:-10.0"
 
     # yawing the base right and pitching it up requires unyawing, but
     # less than 90 degrees, and tilt less than 20; as pitch increases
     # past 45 degrees, pan becomes responsible for elevation and tilt
     # for azimuth
     att = RPY(0, 30, 90)
-    assert str(compose(att, azel)) == "p: -79.7 t: 17.2"
+    assert str(necessary_pt(att, azel)) == "p: -79.7 t: 17.2"
